3c6ab740652498dfe90099c78bd2abcc3373daad
Bruno <bruno.cnasc@gmail.com>
2023-08-10 16:58:18 +0100

    função contato reativada

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-<script>
-    const form = document.getElementById('whatsapp-form');
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      
-      const userName = document.getElementById('user-name').value;
-      const messageText = document.getElementById('message-text').value;
-      
-      const phoneNumber = '+351962836059'; // Substitua pelo número desejado
-      
-      const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(messageText)}`;
-      
-      window.open(whatsappLink, '_blank');
-    });
-  </script>
+const form = document.getElementById('whatsapp-form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const messageText = document.getElementById('message').value;
+
+  const phoneNumber = '+351962836059'; // Substitua pelo número desejado
+
+  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(messageText)}`;
+
+  window.open(whatsappLink, '_blank');
+});
