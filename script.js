55ddc9153f0b1dbfda8be03962487022810dfe08
Bruno <bruno.cnasc@gmail.com>
2023-08-31 09:23:56 +0100

    update no form

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const form = document.getElementById('whatsapp-form');
 form.addEventListener('submit', function (event) {
   event.preventDefault();
-
   const messageText = document.getElementById('message').value;
+  const userName = document.getElementById('username').value;
 
   const phoneNumber = '+351962836059'; // Substitua pelo número desejado
 
-  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(messageText)}`;
+  const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=Olá, me chamo ${encodeURIComponent(userName)}. ${encodeURIComponent(messageText)}`;
 
   window.open(whatsappLink, '_blank');
 });
